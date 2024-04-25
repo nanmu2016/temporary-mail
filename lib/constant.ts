539fc0bd8c67dc6048d886d6e693512b242801b5
楠木 <167427288+nanmu2016@users.noreply.github.com>
2024-04-25 14:39:15 +0800

    Update constant.ts

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,4 +1,4 @@
-export const DOMAIN_LIST = ["@isco.eu.org", "@sliu.eu.org"];
+export const DOMAIN_LIST = ["@ruohan.eu.org", "@ruo521.com"];
 
 export const REFRESH_SECONDS =
   process.env.NODE_ENV === "development" ? 1000 : 10;
